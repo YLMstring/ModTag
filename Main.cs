966f576433ab877fddc3683347d8844227855efd
YLMstring <61271096+YLMstring@users.noreply.github.com>
2024-08-14 21:36:59 +0800

    Initial commit

---
+++ Main.cs
@@ -0,0 +1,65 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using UnityModManagerNet;
+using BlueprintCore.Utils;
+using Kingmaker.Blueprints.JsonSystem;
+
+namespace ModTag;
+
+#if DEBUG
+[EnableReloading]
+#endif
+public static class Main {
+    internal static Harmony HarmonyInstance;
+    internal static UnityModManager.ModEntry.ModLogger log;
+
+    public static bool Load(UnityModManager.ModEntry modEntry) {
+        log = modEntry.Logger;
+#if DEBUG
+        modEntry.OnUnload = OnUnload;
+#endif
+        modEntry.OnGUI = OnGUI;
+        HarmonyInstance = new Harmony(modEntry.Info.Id);
+        HarmonyInstance.PatchAll(Assembly.GetExecutingAssembly());
+        return true;
+    }
+
+    public static void OnGUI(UnityModManager.ModEntry modEntry) {
+
+    }
+
+#if DEBUG
+    public static bool OnUnload(UnityModManager.ModEntry modEntry) {
+        HarmonyInstance.UnpatchAll(modEntry.Info.Id);
+        return true;
+    }
+#endif
+    [HarmonyPatch(typeof(BlueprintsCache))]
+    public static class BlueprintsCaches_Patch {
+        private static bool Initialized = false;
+
+        [HarmonyPriority(Priority.First)]
+        [HarmonyPatch(nameof(BlueprintsCache.Init)), HarmonyPostfix]
+        public static void Init_Postfix() {
+            try {
+                if (Initialized) {
+                    log.Log("Already initialized blueprints cache.");
+                    return;
+                }
+                Initialized = true;
+
+                log.Log("Patching blueprints.");
+                // Insert your mod's patching methods here
+                // Example
+                // SuperAwesomeFeat.Configure()
+            } catch (Exception e) {
+                log.Log(string.Concat("Failed to initialize.", e));
+            }
+        }
+    }
+}
---
+++ TagStuff.cs
@@ -0,0 +1,33 @@
+﻿using HarmonyLib;
+using Kingmaker.Localization.Shared;
+using Kingmaker.Localization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModTag
+{
+    [HarmonyPatch(typeof(LocalizedString), nameof(LocalizedString.LoadString))]
+    internal class DualLanguageMode3
+    {
+        static void Postfix(ref string __result, ref LocalizedString __instance)
+        {
+            try
+            {
+                if (!__result.Contains(".") && !__result.Contains("。")) { return; }
+                string actualKey = __instance.GetActualKey();
+                LocalizationPack.StringEntry stringEntry;
+                if (!pack1.m_Strings.TryGetValue(actualKey, out stringEntry) || !pack2.m_Strings.TryGetValue(actualKey, out stringEntry))
+                {
+                    __result += " (mod)";
+                }
+            }
+            catch { }
+        }
+
+        private static LocalizationPack pack1 = LocalizationManager.LoadPack(Locale.enGB);
+        private static LocalizationPack pack2 = LocalizationManager.LoadPack(Locale.zhCN);
+    }
+}
